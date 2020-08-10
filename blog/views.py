6caeb565bd1299ae24b4f900c9ea16c441e1906d
Rachel <rjcrter11@gmail.com>
2020-08-10 12:45:09 -0500

    adds blog app

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 10, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 11, 2020'
+    },
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
